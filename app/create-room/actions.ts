36f76c756a97f12caabfd18e64ac31b3772f56df
Vukasin Mladzic <vukasin.ml2@gmail.com>
2024-03-27 15:24:30 +0100

    working on displaying all of the rooms on the homepage

--- app/create-room/actions.ts
+++ app/create-room/actions.ts
@@ -3,6 +3,7 @@
 import { db } from "@/db";
 import { room, Room } from "@/db/schema";
 import { getSession } from "@/lib/auth";
+import { revalidatePath } from "next/cache";
 
 export async function createRoomAction(roomData: Omit<Room, "userId" | "id">) {
   const session = await getSession();
@@ -11,4 +12,6 @@ export async function createRoomAction(roomData: Omit<Room, "userId" | "id">) {
     throw new Error("You must be logged in to create a room!");
   }
   await db.insert(room).values({ ...roomData, userId: session.user.id });
+
+  revalidatePath("/");
 }
--- app/header.tsx
+++ app/header.tsx
@@ -3,19 +3,66 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
+
+function AccountDropdown() {
+  const session = useSession();
+
+  const isLoggedIn = !!session.data;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user.image ?? ""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" />
+            Sign out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogInIcon className="mr-2" />
+            Sign in
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export function Header() {
   const session = useSession();
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign out</Button>
-        ) : (
-          <Button onClick={() => signIn("google")}>Sign in</Button>
-        )}
-        <ModeToggle />
+    <header className="bg-gray-200 dark:bg-gray-900  container mx-auto py-2">
+      <div className="flex justify-between items-center">
+        <Link href="/" className="flex gap-2 items-center text-xl hover:underline">
+          <Image src="/icon.png" width="60" height="60" alt="the application logo" />
+          DevFinder
+        </Link>
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
